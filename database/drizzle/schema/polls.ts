edb4681cc68087d95e39f817079dab54546e320b
hato810424 <yukkuri.hatopoppo@gmail.com>
2024-10-29 13:33:47 +0900

    fix: hono rpcではDate型を渡せないためカラムを変更

--- database/drizzle/schema/polls.ts
+++ database/drizzle/schema/polls.ts
@@ -1,11 +1,18 @@
 import { FormComponent } from "@/pages/poll/create/+Page";
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const pollTable = sqliteTable('polls', {
   id: text().notNull().unique().primaryKey(),
   author_id: text().notNull(),
-  data: text({ mode: "json" }).notNull().$type<FormComponent>()
+  data: text({ mode: "json" }).notNull().$type<FormComponent>(),
+  // https://orm.drizzle.team/docs/guides/timestamp-default-value
+  created_at: integer().notNull().default(sql`(unixepoch())`),
+  closed_at: integer(),
 })
+
+// Hono RPCでDate型を渡せない問題について
+// https://github.com/honojs/hono/issues/1800
 
 export type PollItem = typeof pollTable.$inferSelect;
 export type PollInsert = typeof pollTable.$inferInsert;
