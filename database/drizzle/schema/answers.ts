fd39dcb2af645e9478d3a9b29e1c24867f75454f
hato810424 <yukkuri.hatopoppo@gmail.com>
2024-10-29 17:28:51 +0900

    fix: uniqueのせいで1ユーザーにつき1回答権しかなったのを修正

--- database/drizzle/schema/answers.ts
+++ database/drizzle/schema/answers.ts
@@ -1,11 +1,13 @@
 import { FormAnswerData } from "@/pages/polls/@id/Poll";
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, unique } from "drizzle-orm/sqlite-core";
 
 export const answerTable = sqliteTable('answers', {
   poll_id: text().notNull(),
-  user_id: text().notNull().unique(),
+  user_id: text().notNull(),
   data: text({ mode: "json" }).notNull().$type<FormAnswerData[]>()
-})
+}, (t) => ({
+  poll_user_unique: unique().on(t.poll_id, t.user_id),
+}))
 
 export type answerItem = typeof answerTable.$inferSelect;
 export type answerInsert = typeof answerTable.$inferInsert;
