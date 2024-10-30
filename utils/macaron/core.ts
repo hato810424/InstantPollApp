84169208e4b5dc302c94853ca24d94980e2fc561
hato810424 <yukkuri.hatopoppo@gmail.com>
2024-10-30 20:59:44 +0900

    remove:

--- utils/macaron/core.ts
+++
@@ -1,6 +0,0 @@
-import * as macron from "@macaron-css/core";
-
-export const {
-  style,
-  globalStyle
-} = macron;
--- utils/macaron/react.ts
+++
@@ -1,5 +0,0 @@
-import * as macron from "@macaron-css/react";
-
-export const {
-  styled,
-} = macron;
