75e335e8ccd4d4cdeb9cba5fb3b5f540e42edc90
hato810424 <yukkuri.hatopoppo@gmail.com>
2024-10-30 19:13:59 +0900

    style: 存在しない質問ページのh1の太さをnormalに

--- pages/polls/notfound/+Page.tsx
+++ pages/polls/notfound/+Page.tsx
@@ -1,9 +1,10 @@
+import { normalWeight } from "@/utils/styles";
 import { Container } from "@mantine/core";
 
 export default function Page() {
   return (
-    <Container size="md">
-      <h1>存在しない質問のようです</h1>
+    <Container size="md" p={0}>
+      <h1 className={normalWeight}>存在しない質問のようです</h1>
     </Container>
   )
 }
